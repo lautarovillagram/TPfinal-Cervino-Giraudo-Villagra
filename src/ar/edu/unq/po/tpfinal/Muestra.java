2934f7e474ecde1dcdf1199230acd4635d430ed6
Lauta <Lauta@DESKTOP-BQ3238V>
2022-05-13 16:34:53 -0300

    Clase Ubicacion y variables clase Muestra

--- src/ar/edu/unq/po/tpfinal/Muestra.java
+++ src/ar/edu/unq/po/tpfinal/Muestra.java
@@ -1,5 +1,14 @@
 package ar.edu.unq.po.tpfinal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Muestra {
+	private Especie especie;
+	private Foto foto;
+	private Ubicacion ubicacion;
+	private Usuario usuarioRecolectador;
+	private List<Especie> opinionesRegulares = new ArrayList<Especie>();
+	private List<Especie> opinionesExpertas = new ArrayList<Especie>();
 
 }
