422b2a54b5e7aded5f74b59ac9b836c6874d5521
Lauta <Lauta@DESKTOP-BQ3238V>
2022-05-19 23:46:41 -0300

    metodos de Usuario para completar

--- src/ar/edu/unq/po/tpfinal/Usuario.java
+++ src/ar/edu/unq/po/tpfinal/Usuario.java
@@ -1,5 +1,18 @@
 package ar.edu.unq.po.tpfinal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
+
+	List<Muestra> muestrasEnviadas = new ArrayList<Muestra>();
+
+	public void enviarMuestra(Muestra muestraAEnviar) {
+		// TODO
+	}
+
+	public void opinarMuestra(Muestra muestraAOpinar, String especie) {
+		// TODO
+	}
 
 }
