54988798972dbe7f640a2a652221bcdd9d32662b
Lauta <Lauta@DESKTOP-BQ3238V>
2022-06-09 14:06:40 -0300

    cambios en los test para que implemente el observer

--- test/ar/edu/unq/po/tpfinal/MuestraTestCase.java
+++ test/ar/edu/unq/po/tpfinal/MuestraTestCase.java
@@ -10,24 +10,41 @@ import java.time.LocalDateTime;
 
 public class MuestraTestCase {
 	private Muestra muestra;
-	private Usuario u;
-	private Usuario u2;
+	private UsuarioRegular u;
+	private UsuarioRegular u2;
 	private Usuario u3;
 	private Usuario u4;
+	private UsuarioRegular u5;
+	private ActualizadorDeCategoria observer;
+	private ActualizadorDeCategoria observer2;
+
 	
 	@BeforeEach
 	public void setUp() {
 		LocalDateTime fecha = LocalDateTime.now();
 		Ubicacion ubicacion = mock(Ubicacion.class);
 		Foto foto = mock(Foto.class);
+		ActualizadorDeCategoria observer3 = mock(ActualizadorDeCategoria.class);
+		u5 = new UsuarioRegular();
+		u5.setObservador(observer3);
+		observer3.setObservable(u5);
 		
-		muestra = new Muestra("Vinchuca Infestans", foto, ubicacion, new UsuarioRegular(), fecha);
+		
+		muestra = new Muestra("Vinchuca Infestans", foto, ubicacion, u5, fecha);
+		observer = new ActualizadorDeCategoria();
+		observer2 = new ActualizadorDeCategoria();
 		
 		u  = new UsuarioRegular();
 		u2 = new UsuarioRegular();
+		u.setObservador(observer);
+		u2.setObservador(observer2);
+		observer.setObservable(u);
+		observer2.setObservable(u2);
+		
 		
 		u3 = new UsuarioExperto();
 		u4 = new UsuarioExperto();
+		
 	}
 	
 	@Test
