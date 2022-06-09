1eeea947337ce58dde166826051fbeb29036fae8
Lauta <Lauta@DESKTOP-BQ3238V>
2022-06-09 14:05:19 -0300

    se agrego esto que faltaba a la main

---
+++ src/ar/edu/unq/po/tpfinal/ActualizadorDeCategoria.java
@@ -0,0 +1,41 @@
+package ar.edu.unq.po.tpfinal;
+
+import java.util.List;
+
+public class ActualizadorDeCategoria implements ObserverCategoria {
+	private UsuarioRegular observable;
+
+	public UsuarioRegular getObservable() {
+		return observable;
+	}
+
+	public void setObservable(UsuarioRegular observable) {
+		this.observable = observable;
+	}
+
+	/*
+	 * verifica la cantidad de muestras enviadas y opiniones enviadas, si 10
+	 * muestras y 20 opiniones fueron enviadas en menos de 30 dias, cambia la
+	 * categoria de usuario regular a experto
+	 */
+
+	public List<Muestra> muestrasEnviadasLosUltimos30Dias() {
+		return this.getObservable().getMuestrasEnviadas().stream().filter(m -> m.tieneMenosDe30Dias()).toList();
+	}
+
+	public List<Opinion> opinionesEnviadasLosUltimos30Dias() {
+		return this.getObservable().getOpinionesEnviadas().stream().filter(o -> o.tieneMenosDe30Dias()).toList();
+	}
+
+	public void actualizar() {
+
+		if (this.muestrasEnviadasLosUltimos30Dias().size() > 10
+				&& this.opinionesEnviadasLosUltimos30Dias().size() > 20) {
+			this.getObservable().setSubioAExperto(true);
+
+		} else {
+			this.getObservable().setSubioAExperto(false);
+		}
+	}
+
+}
