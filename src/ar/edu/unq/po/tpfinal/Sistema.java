50ed052a0464e0e67efc63047f56a7b5b03c70ad
Nicolas Cervino <nicolascervino2@gmail.com>
2022-06-29 14:05:21 -0300

    Se añadio un metodo buscar que delega en el Buscador

--- src/ar/edu/unq/po/tpfinal/Sistema.java
+++ src/ar/edu/unq/po/tpfinal/Sistema.java
@@ -2,19 +2,14 @@ package ar.edu.unq.po.tpfinal;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Sistema {
 	private List<Usuario> usuarios = new ArrayList<>();
 	private List<Muestra> muestras = new ArrayList<>();
 	private List<ZonaDeCobertura> zonasDeCobertura = new ArrayList<>();
-	private Buscador buscador;
-	private ActualizadorDeCategoria recategorizador;
-
-	public Sistema() {
-		this.setBuscador(new Buscador(this.getMuestras()));
-	}
-
+	private ActualizadorDeCategoria recategorizador = new ActualizadorDeCategoria();
+	private Buscador buscador = new Buscador();
+	
 	// Agrega un usuario al Sistema
 	public void agregarUsuario(Usuario u) {
 		this.getUsuarios().add(u);
@@ -25,20 +20,26 @@ public class Sistema {
 		this.getZonasDeCobertura().add(z);
 	}
 
-	// Agrega una muestra al sistema y a las zonas que pertenezca
+	// Agrega una muestra al sistema, al buscador y a las zonas que pertenezca
 	public void agregarMuestra(Muestra m) {
 		this.getMuestras().add(m);
+		this.getBuscador().agregarMuestra(m);
 		for (ZonaDeCobertura z : this.getZonasDeCobertura()) {
 			z.agregarMuestra(m); // agregarMuestra se encarga de comprobar si la muestra pertenece antes de
-									// agregarla
+								 // agregarla
 		}
 	}
 
 	public void recategorizar() {
 		this.getUsuarios().stream().forEach(u -> recategorizador.actualizarCategoria(u));
-
 	}
-
+	
+	
+	// Ejecuta la busqueda que recibe por parametro
+	public List<Muestra> buscar(Busqueda b) {
+		return this.getBuscador().buscar(b);
+	}
+	
 	// Getters y Setters
 	public List<Usuario> getUsuarios() {
 		return usuarios;
@@ -51,12 +52,8 @@ public class Sistema {
 	public List<ZonaDeCobertura> getZonasDeCobertura() {
 		return zonasDeCobertura;
 	}
-
+	
 	public Buscador getBuscador() {
 		return buscador;
-	}
-
-	private void setBuscador(Buscador b) {
-		this.buscador = b;
 	}
 }
