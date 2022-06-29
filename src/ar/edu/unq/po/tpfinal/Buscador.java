4afc6cdbae110c427b2fae6d32f50eca6c8cbea6
Nicolas Cervino <nicolascervino2@gmail.com>
2022-06-29 14:00:04 -0300

    Se modifico el buscador para que utilice instancias de Busqueda

--- src/ar/edu/unq/po/tpfinal/Buscador.java
+++ src/ar/edu/unq/po/tpfinal/Buscador.java
@@ -3,62 +3,49 @@ package ar.edu.unq.po.tpfinal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Buscador {
-	private List<Muestra> muestras;
-
-	public Buscador(List<Muestra> muestras) {
-		this.setMuestras(muestras);
+	private List<Muestra> muestras = new ArrayList<>();
+	
+	// Ejecuta la busqueda que recibe por parametro
+	public List<Muestra> buscar(Busqueda b) {
+		return b.buscar();
 	}
 	
-	// Recibe por parametros 2 listas que son el resultado de aplicar algun filtro de busqueda
-	// y devuelve las que cumplan con ambos criterios
-	public List<Muestra> buscarPor_Y_(List<Muestra> filtro1, List<Muestra> filtro2) {
-		// Si es un AND tengo que crear una lista con solo los elementos que se repitan en ambas listas
-		List<Muestra> resultado = new ArrayList<>();
-		for(Muestra m: filtro1) {
-			if (filtro2.contains(m)) {
-				resultado.add(m);
-			}
-		}
-		return resultado;
+	// Permite agregar una muestra al buscador, el sistema es responsable de llamar
+	// a este metodo cada vez que se agregue una muestra nueva
+	public void agregarMuestra(Muestra m) {
+		this.getMuestras().add(m);
 	}
 	
-	// Recibe por parametros 2 listas que son el resultado de aplicar algun filtro de busqueda
-	// y devuelve las que cumplan con alguno de los 2 criterios
-	public List<Muestra> buscarPor_O_(List<Muestra> filtro1, List<Muestra> filtro2) {
-		// Si es un OR tengo que combinar ambas listas
-		List<Muestra> resultado = filtro1;
-		for(Muestra m: filtro2) {
-			if (! resultado.contains(m)) { 
-				resultado.add(m);
-			}
-		}
-		return resultado;
+	// Crea una instancia de busqueda AND con las busquedas dadas por parametro
+	public Busqueda buscarPor_Y_(Busqueda b1, Busqueda b2) {
+		return new BusquedaAND(b1, b2, this.getMuestras());
 	}
 	
-	public List<Muestra> buscarPorFechaDeCreacion(LocalDateTime fecha) {
-		return this.getMuestras().stream().filter(m -> m.getFecha().equals(fecha)).collect(Collectors.toList());
+	// Crea una instancia de busqueda AND con las busquedas dadas por parametro
+	public Busqueda buscarPor_O_(Busqueda b1, Busqueda b2) {
+		return new BusquedaOR(b1, b2, this.getMuestras());
 	}
 	
-	public List<Muestra> buscarPorFechaDeUltimaVotacion(LocalDateTime fecha) {
-		return this.getMuestras().stream().filter(m -> m.getFechaUltimaVotacion().equals(fecha)).collect(Collectors.toList());
+	// Crea una instancia de busqueda con el criterio dado por parametro
+	public Busqueda buscarPorFechaDeCreacion(LocalDateTime fecha) {
+		return new BusquedaPorFechaDeCreacion(fecha, this.getMuestras());
 	}
 	
-	public List<Muestra> buscarPorTipoDeInsecto(String especie) {
-		return this.getMuestras().stream().filter(m -> m.resultadoActual().equals(especie)).collect(Collectors.toList());
+	// Crea una instancia de busqueda con el criterio dado por parametro
+	public Busqueda buscarPorFechaDeUltimaVotacion(LocalDateTime fecha) {
+		return new BusquedaPorFechaUltimaVotacion(fecha, this.getMuestras());
 	}
 	
-	public List<Muestra> buscarPorNivelDeVerificacion(String nivel) {
-		switch(nivel) {
-		case "verificada": 
-			return this.getMuestras().stream().filter(m -> m.estaVerificada()).collect(Collectors.toList());
-		case "votada": 
-			return this.getMuestras().stream().filter(m -> !m.estaVerificada()).collect(Collectors.toList());
-		}
-		System.out.println("No es un nivel valido");
-		return muestras;
+	// Crea una instancia de busqueda con el criterio dado por parametro
+	public Busqueda buscarPorTipoDeInsecto(String especie) {
+		return new BusquedaPorTipoDeInsecto(especie, this.getMuestras());
+	}
+	
+	// Crea una instancia de busqueda con el criterio dado por parametro
+	public Busqueda buscarPorNivelDeVerificacion(String nivel) {
+		return new BusquedaPorNivelDeVerificacion(nivel, this.getMuestras());
 	}
 	
 	// Getter y Setters
