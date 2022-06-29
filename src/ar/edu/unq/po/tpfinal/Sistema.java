b783c00f3ef30be9ecb6477da814b0a3ffce8923
Lauta <Lauta@DESKTOP-BQ3238V>
2022-06-29 16:41:13 -0300

    se agrega las opiniones al sistema

--- src/ar/edu/unq/po/tpfinal/Sistema.java
+++ src/ar/edu/unq/po/tpfinal/Sistema.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class Sistema {
 	private List<Usuario> usuarios = new ArrayList<>();
 	private List<Muestra> muestras = new ArrayList<>();
+	private List<Opinion> opiniones = new ArrayList<>();
 	private List<ZonaDeCobertura> zonasDeCobertura = new ArrayList<>();
 	private ActualizadorDeCategoria recategorizador = new ActualizadorDeCategoria();
 	private Buscador buscador = new Buscador();
-	
+
 	// Agrega un usuario al Sistema
 	public void agregarUsuario(Usuario u) {
 		this.getUsuarios().add(u);
@@ -26,20 +27,27 @@ public class Sistema {
 		this.getBuscador().agregarMuestra(m);
 		for (ZonaDeCobertura z : this.getZonasDeCobertura()) {
 			z.agregarMuestra(m); // agregarMuestra se encarga de comprobar si la muestra pertenece antes de
-								 // agregarla
+									// agregarla
 		}
+	}
+
+	public void agregarOpinion(Opinion o) {
+		this.getOpiniones().add(o);
+	}
+
+	public void setRecategorizador(ActualizadorDeCategoria actualizador) {
+		this.recategorizador = actualizador;
 	}
 
 	public void recategorizar() {
 		this.getUsuarios().stream().forEach(u -> recategorizador.actualizarCategoria(u));
 	}
-	
-	
+
 	// Ejecuta la busqueda que recibe por parametro
 	public List<Muestra> buscar(Busqueda b) {
 		return this.getBuscador().buscar(b);
 	}
-	
+
 	// Getters y Setters
 	public List<Usuario> getUsuarios() {
 		return usuarios;
@@ -52,7 +60,11 @@ public class Sistema {
 	public List<ZonaDeCobertura> getZonasDeCobertura() {
 		return zonasDeCobertura;
 	}
-	
+
+	public List<Opinion> getOpiniones() {
+		return opiniones;
+	}
+
 	public Buscador getBuscador() {
 		return buscador;
 	}
