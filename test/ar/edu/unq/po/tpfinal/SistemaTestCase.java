d7c146f18c48660234a8f8bd8dc86be175e89a37
Nicolas Cervino <nicolascervino2@gmail.com>
2022-07-02 19:59:52 -0300

    Se arreglaron algunos test

--- test/ar/edu/unq/po/tpfinal/SistemaTestCase.java
+++ test/ar/edu/unq/po/tpfinal/SistemaTestCase.java
@@ -19,16 +19,22 @@ public class SistemaTestCase {
 	private Muestra m3;
 	private Usuario u;
 	private ZonaDeCobertura z;
+	private Foto foto;
+	private Ubicacion ubicacion;
+	
 	
 	@BeforeEach
 	public void setUp() {
+		foto = mock(Foto.class);
+		ubicacion = mock(Ubicacion.class);
+		u = mock(UsuarioRegular.class);
+			
 		m1 = mock(Muestra.class);
 		m2 = mock(Muestra.class);
 		m3 = mock(Muestra.class);
 		
 		z = mock(ZonaDeCobertura.class);
 		
-		u = mock(Usuario.class);
 		
 		sistema = new Sistema();
 	}
@@ -47,10 +53,14 @@ public class SistemaTestCase {
 	
 	@Test
 	public void testAgregarMuestra() {
+		m1 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.now());
+		m2 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.now());
+		m3 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.now());
+		
 		sistema.agregarZonaDeCobertura(z);
 				
 		sistema.agregarMuestra(m1);
-		sistema.agregarMuestra(m2);
+		sistema.agregarMuestra(m2);	
 		
 		// Verifico que la zona reciba el mensaje agregarMuestra
 		verify(z).agregarMuestra(m1);
@@ -65,9 +75,10 @@ public class SistemaTestCase {
 	public void testBuscarPorFechaDeCreacion() {
 		LocalDateTime fechaABuscar = LocalDateTime.of(2022, 05, 15, 15, 55);
 		// Se espera que la busqueda devuelva solo las muestras m1 y m2
-		when(m1.getFecha()).thenReturn(fechaABuscar);
-		when(m2.getFecha()).thenReturn(fechaABuscar);
-		when(m3.getFecha()).thenReturn(LocalDateTime.of(2021, 04, 25, 14, 30));
+		m1 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, fechaABuscar);
+		m2 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, fechaABuscar);
+		m3 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.of(2021, 04, 25, 14, 30));
+		
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
@@ -85,9 +96,9 @@ public class SistemaTestCase {
 	public void testBuscarPorFechaDeUltimaVotacion() {
 		LocalDateTime fechaABuscar = LocalDateTime.of(2022, 05, 15, 15, 55);
 		// Se espera que la busqueda devuelva solo las muestras m1 y m2
-		when(m1.getFechaUltimaVotacion()).thenReturn(fechaABuscar);
-		when(m2.getFechaUltimaVotacion()).thenReturn(fechaABuscar);
-		when(m3.getFechaUltimaVotacion()).thenReturn(LocalDateTime.of(2021, 04, 25, 14, 30));
+		m1 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, fechaABuscar);
+		m2 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, fechaABuscar);
+		m3 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.of(2021, 04, 25, 14, 30));
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
@@ -103,9 +114,9 @@ public class SistemaTestCase {
 	
 	@Test
 	public void testBuscarPorTipoDeInsecto() {
-		when(m1.resultadoActual()).thenReturn("Vinchuca Infestans");
-		when(m2.resultadoActual()).thenReturn("Chinche Foliada");
-		when(m3.resultadoActual()).thenReturn("imagen poco clara");
+		m1 = new Muestra("Vinchuca Infestans", foto, ubicacion, u, LocalDateTime.now());
+		m2 = new Muestra("Chinche Foliada", foto, ubicacion, u, LocalDateTime.now());
+		m3 = new Muestra("imagen poco clara", foto, ubicacion, u, LocalDateTime.now());
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
@@ -119,13 +130,23 @@ public class SistemaTestCase {
 	
 	@Test
 	public void testBuscarPorNivelDeVerificacionVerificada() {
-		when(m1.estaVerificada()).thenReturn(true);
-		when(m2.estaVerificada()).thenReturn(false);
-		when(m3.estaVerificada()).thenReturn(true);
+		when(m1.getEspecie()).thenReturn("Vinchuca Infestans");
+		when(m2.getEspecie()).thenReturn("Vinchuca Infestans");
+		when(m3.getEspecie()).thenReturn("Vinchuca Infestans");
+		
+		when(m1.getUsuarioRecolectador()).thenReturn(u);
+		when(m2.getUsuarioRecolectador()).thenReturn(u);
+		when(m3.getUsuarioRecolectador()).thenReturn(u);	
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
 		sistema.agregarMuestra(m3);
+		
+		// Se espera que el resultado solo devuelva las muestras m1 y m3
+		when(m1.estaVerificada()).thenReturn(true);
+		when(m2.estaVerificada()).thenReturn(false);
+		when(m3.estaVerificada()).thenReturn(true);
+		
 		List<Muestra> resultado = sistema.buscar(sistema.getBuscador().buscarPorNivelDeVerificacion("verificada"));
 		
 		assertTrue(resultado.contains(m1));
@@ -134,14 +155,23 @@ public class SistemaTestCase {
 	}
 	
 	@Test
-	public void testBuscarPorNivelDeVerificacionVotada() {
-		when(m1.estaVerificada()).thenReturn(true);
-		when(m2.estaVerificada()).thenReturn(false);
-		when(m3.estaVerificada()).thenReturn(true);
+	public void testBuscarPorNivelDeVerificacionVotada() {	
+		when(m1.getEspecie()).thenReturn("Vinchuca Infestans");
+		when(m2.getEspecie()).thenReturn("Vinchuca Infestans");
+		when(m3.getEspecie()).thenReturn("Vinchuca Infestans");
+		
+		when(m1.getUsuarioRecolectador()).thenReturn(u);
+		when(m2.getUsuarioRecolectador()).thenReturn(u);
+		when(m3.getUsuarioRecolectador()).thenReturn(u);
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
 		sistema.agregarMuestra(m3);
+		// Se espera que el resultado solo devuelva la muestra m2
+		when(m1.estaVerificada()).thenReturn(true);
+		when(m2.estaVerificada()).thenReturn(false);
+		when(m3.estaVerificada()).thenReturn(true);
+		
 		List<Muestra> resultado = sistema.buscar(sistema.getBuscador().buscarPorNivelDeVerificacion("votada"));
 		
 		assertFalse(resultado.contains(m1));
@@ -151,14 +181,20 @@ public class SistemaTestCase {
 	
 	@Test
 	public void testCombinarCriteriosDeBusquedaOR() {
+		when(m1.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m1.resultadoActual()).thenReturn("Vinchuca Infestans");
 		when(m1.estaVerificada()).thenReturn(true);
-
+		when(m1.getUsuarioRecolectador()).thenReturn(u);
+		
+		when(m2.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m2.resultadoActual()).thenReturn("Vinchuca Infestans");
 		when(m2.estaVerificada()).thenReturn(false);
-
+		when(m2.getUsuarioRecolectador()).thenReturn(u);
+		
+		when(m3.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m3.resultadoActual()).thenReturn("Chinche Foliada");
 		when(m3.estaVerificada()).thenReturn(false);
+		when(m3.getUsuarioRecolectador()).thenReturn(u);
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
@@ -169,6 +205,7 @@ public class SistemaTestCase {
 		
 		Busqueda or = sistema.getBuscador().buscarPor_O_(b1, b2);
 		
+		// Se espera que el resultado contenga a m1 y m2
 		List<Muestra> resultado = sistema.buscar(or);
 		
 		assertTrue(resultado.contains(m1));
@@ -178,14 +215,20 @@ public class SistemaTestCase {
 	
 	@Test
 	public void testCombinar2CriteriosDeBusquedaAND() {
+		when(m1.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m1.resultadoActual()).thenReturn("Vinchuca Infestans");
 		when(m1.estaVerificada()).thenReturn(true);
-
+		when(m1.getUsuarioRecolectador()).thenReturn(u);
+		
+		when(m2.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m2.resultadoActual()).thenReturn("Vinchuca Infestans");
 		when(m2.estaVerificada()).thenReturn(false);
-
+		when(m2.getUsuarioRecolectador()).thenReturn(u);
+		
+		when(m3.getEspecie()).thenReturn("Vinchuca Infestans");
 		when(m3.resultadoActual()).thenReturn("Chinche Foliada");
-		when(m3.estaVerificada()).thenReturn(true);
+		when(m3.estaVerificada()).thenReturn(false);
+		when(m3.getUsuarioRecolectador()).thenReturn(u);
 		
 		sistema.agregarMuestra(m1);
 		sistema.agregarMuestra(m2);
@@ -195,6 +238,7 @@ public class SistemaTestCase {
 		Busqueda b2 = sistema.getBuscador().buscarPorTipoDeInsecto("Vinchuca Infestans"); 
 		Busqueda and = sistema.getBuscador().buscarPor_Y_(b1,b2);
 		
+		// Se espera que el resultado solo contenga a m1
 		List<Muestra> resultado = sistema.buscar(and);
 		
 		assertTrue(resultado.contains(m1));
